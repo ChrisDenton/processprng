0ef9d672f7bd34c08a7aa4c33d6594e276b8b1cc
Chris Denton <chris@chrisdenton.dev>
2024-10-01 12:57:29 +0000

    Add another import

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,22 @@
 type BOOL = i32;
 type PBYTE = *mut u8;
 type SIZE_T = isize;
+type PVOID = *mut ();
+type DWORD = u32;
 
 #[link(name = "bcryptprimitives", kind = "raw-dylib")]
 extern "system" {
     pub fn ProcessPrng(pbData: PBYTE, cbData: SIZE_T) -> BOOL;
+}
+
+#[link(name = "bcryptprimitives", kind = "raw-dylib")]
+extern "system" {
+    pub fn WaitOnAddress(
+        Address: PVOID,
+        CompareAddress: PVOID,
+        AddressSize: SIZE_T,
+        dwMilliseconds: DWORD,
+    ) -> BOOL;
 }
 
 #[panic_handler]
